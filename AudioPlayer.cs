cc3dd5d8d4b10d30e2cdc8dfd8f1c794936de2d8
Shanton Johnson <shantonjohnson98@gmail.com>
2026-03-17 13:54:48 +0200

    edit/added comments

--- AudioPlayer.cs
+++ AudioPlayer.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Media;
 
 namespace CyberSecurityBot
 {
+    // This class is responsible for handling audio playback
     internal class AudioPlayer
     {
+        public static void PlayGreeting()
+        {
+            try
+            {
+                // Creates a SoundPlayer object and loads the WAV file
+                SoundPlayer player = new SoundPlayer("Assets/greeting.wav");
+
+                // Plays the audio file and waits until it finishes before continuing
+                player.PlaySync();
+            }
+            catch (Exception)
+            {
+                // If an error occurs (e.g., file not found), display an error message
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("⚠️ Unable to play greeting audio.");
+                Console.ResetColor();
+            }
+        }
     }
 }
